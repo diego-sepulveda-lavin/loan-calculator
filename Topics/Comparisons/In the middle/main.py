fa9207733fb84265182810e9df0fc74ca9efc74d
Diego Sepulveda <diegosl@hotmail.cl>
2021-09-21 20:44:45 -0300

    comparisons done

---
+++ Topics/Comparisons/In the middle/main.py
@@ -0,0 +1,9 @@
+# don't modify this code
+# a stores an input value
+a = int(input().strip())
+
+# put your code here
+b = 10
+c = 250
+
+print(a < b or a > c)
---
+++ Topics/Comparisons/Very odd/main.py
@@ -0,0 +1,7 @@
+dividend = int(input())
+divisor = int(input())
+
+result = dividend / divisor
+is_result_odd = result % 2 == 1
+
+print(is_result_odd)
